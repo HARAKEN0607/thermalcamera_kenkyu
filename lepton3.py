15f4255405957e46a71ab2fe4a884e0e48fd8cab
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-10-18 12:41:33 +0900

    change

--- lepton3.py
+++ lepton3.py
@@ -2,7 +2,7 @@ import numpy as np
 import ctypes
 import struct
 import time
-import ioctl
+from ioctl_numbers import _IOR, _IOW
 # from fcntl import ioctl
 
 SPI_IOC_MAGIC   = ord("k")  # unicode 107
@@ -12,7 +12,7 @@ messages = 60   # RAWS
 __xmit_struct = struct.Struct("=QQIIHBBI") # struct original
 __msg_size = __xmit_struct.size # 8+8+4+4+2+1+1+4=32
 
-iow = ioctl._IOW(SPI_IOC_MAGIC, 0, __msg_size)
+iow = ioctl_IOW(SPI_IOC_MAGIC, 0, __msg_size)
 
 
 
