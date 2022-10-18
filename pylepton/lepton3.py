79c1a23c9b360da5a890d1f90331bd990c60c0e5
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-10-18 13:52:06 +0900

    add file

---
+++ pylepton/lepton3.py
@@ -0,0 +1,20 @@
+import numpy as np
+import ctypes
+import struct
+import time
+import ioctl
+import _IOW
+# from fcntl import ioctl
+
+SPI_IOC_MAGIC   = ord("k")  # unicode 107
+
+messages = 60   # RAWS
+
+__xmit_struct = struct.Struct("=QQIIHBBI") # struct original
+__msg_size = __xmit_struct.size # 8+8+4+4+2+1+1+4=32
+
+iow = _IOW(SPI_IOC_MAGIC, 0, __msg_size)
+
+
+
+# iow = _IOW(SPI_IOC_MAGIC, 0, )
