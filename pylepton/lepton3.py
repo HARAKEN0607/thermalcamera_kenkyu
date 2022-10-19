7d5c5c24ed6562d1c1de8fbc6430701841c1b42c
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-10-19 14:54:17 +0900

    change codes

--- pylepton/lepton3.py
+++ pylepton/lepton3.py
@@ -2,23 +2,23 @@ import numpy as np
 import ctypes
 import struct
 import time
-import ioctl
-# from ioctl_numbers import _IOR, _IOW
 import ioctl_numbers
-
 from fcntl import ioctl
 
-SPI_IOC_MAGIC   = ord("k")  # unicode 107
+SPI_IOC_MAGIC = ord("k")  # unicode 107
+ROWS = 60
 
-messages = 60   # RAWS
+messages = ROWS   # RAWS=60
 
 __xmit_struct = struct.Struct("=QQIIHBBI") # struct original
 __msg_size = __xmit_struct.size # 8+8+4+4+2+1+1+4=32
-
 iow = ioctl_numbers._IOW(SPI_IOC_MAGIC, 0, __msg_size)
 
-print(iow)
+__handle = open("/dev/spidev0.0", "wb+", buffering=0) # binary writing
+__xmit_buf = np.zeros((__msg_size * ROWS), dtype=np.uint8)
+ioctl(__handle, iow, __xmit_buf, True)
 
 
 
-# iow = _IOW(SPI_IOC_MAGIC, 0, )
+
+
