f3b8bd523e77ef97fd623112b6bfc53ed956329f
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-10-19 16:00:08 +0900

    change codes

--- pylepton/lepton3.py
+++ pylepton/lepton3.py
@@ -23,7 +23,9 @@ ioctl(__handle, iow, __xmit_buf, True) # writing to camera
 __capture_buf = np.zeros((ROWS, VOSPI_FRAME_SIZE, 1), dtype=np.uint16) # [82行　1列]の[0]が60個
 
 # __capture_buf[0]=0*82        0x000f=[0*14 1]
-while (__capture_buf[0] & 0x000f) == 0x000f: # [0*15] == [0*14 1] repeat 14?
+# [0*15] == [0*14 1] repeat 14?
+
+while (__capture_buf[0] & 0x000f) == 0x000f:
     ioctl(__handle, iow, __xmit_buf, True)
 
 messages -= 1 # messages(60)-1
