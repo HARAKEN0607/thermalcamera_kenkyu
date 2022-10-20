4412c4a122631dca473fb8d08993fa4d1d5f15e8
HARAKEN0607 <113326732+HARAKEN0607@users.noreply.github.com>
2022-10-20 17:14:57 +0900

    change codes

--- pylepton/lepton3_self.py
+++ pylepton/lepton3_self.py
@@ -147,4 +147,4 @@ with Lepton() as l:
 
 cv2.normalize(a, a, 0, 65535, cv2.NORM_MINMAX) # extend contrast
 np.right_shift(a, 8, a) # fit data into 8 bits
-cv2.imwrite("home/oi/thermalcamera_kenkyu/output.jpg", np.uint8(a)) # write it!
+cv2.imwrite("home/pi/thermalcamera_kenkyu/output.jpg", np.uint8(a)) # write it!
